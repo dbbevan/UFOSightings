dc7700fa393f5e7b9b07f1a9f19aa87ffc5bd110
Knight.zhou <YoungWelle@gmail.com>
2013-02-20 13:44:47 +0800

    ADDED:Constants file"

---
+++ UFOSightings/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  UFOSightings
+//
+//  Created by zhou Yangbo on 13-2-20.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
---
+++ UFOSightings/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  UFOSightings
+//
+//  Created by zhou Yangbo on 13-2-20.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
