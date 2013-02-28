a9c3ef58fb7023001aa7d4b8ae136d6cebe5f43d
Knight.zhou <YoungWelle@gmail.com>
2013-02-28 11:08:44 +0800

    CHANGED:more testing code on Python parse json

--- DataAnalysis/src/JSONtest.py
+++ DataAnalysis/src/JSONtest.py
@@ -2,7 +2,7 @@ import json
 #print( json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]) )
 
 JSON_FILE_PATH = '../datasets/AllSightings.json'
-#Load json file to string
+#Load json file to string,decode
 json_raw_data = open(JSON_FILE_PATH)
 json_data = json.load(json_raw_data)
 ##print
@@ -10,3 +10,9 @@ print(json_data)
 #Read string
 for key, value in json_data.iteritems():
     print key,value
+##
+json_data_results = json_data['results']
+print('results:',json_data_results)
+print('results len:',len(json_data_results))
+print(json_data_results[0])
+print(json_data_results[0]["description"])
