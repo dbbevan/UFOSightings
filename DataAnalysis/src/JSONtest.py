1823e29e5769e3a2f5c4bc542e3f48cb10308d1d
Knight.zhou <YoungWelle@gmail.com>
2013-02-28 10:34:54 +0800

    JSON parse testing on Python side

--- DataAnalysis/src/JSONtest.py
+++ DataAnalysis/src/JSONtest.py
@@ -1,2 +1,12 @@
 import json
-print( json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]) )
+#print( json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]) )
+
+JSON_FILE_PATH = '../datasets/AllSightings.json'
+#Load json file to string
+json_raw_data = open(JSON_FILE_PATH)
+json_data = json.load(json_raw_data)
+##print
+print(json_data)
+#Read string
+for key, value in json_data.iteritems():
+    print key,value
